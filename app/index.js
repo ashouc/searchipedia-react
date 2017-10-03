0e6af13bd78d6e92f44a18aa28470b9eeff244a7
Alexandra Shoucri <alexshoucri@gmail.com>
2017-10-03 15:43:52 -0400

    Add CSS file and webpack bundlers

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/App');
+require('../css/styles.css');
 
 ReactDOM.render(<App />, document.getElementById('app'));
